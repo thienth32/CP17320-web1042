cb25a9f5fbca52cdf5da59d495910bf62b616dc2
thienth32 <64114903+thienth32@users.noreply.github.com>
2022-04-03 17:36:43 +0700

    update

---
+++ bai3/main.js
@@ -0,0 +1,5 @@
+function init(){
+    let soA = prompt('Nhập số a');
+    let soB = prompt('Nhập số b');
+    alert(parseInt(soA) + parseInt(soB));
+}
---
+++ lab4/data.js
@@ -0,0 +1,20 @@
+var products = [
+    {
+        id: 1,
+        name: "iphone 13 promax",
+        price: 1000,
+        quantity: 0
+    },
+    {
+        id: 2,
+        name: "Samsung galaxy s21",
+        price: 1500,
+        quantity: 2
+    },
+    {
+        id: 3,
+        name: "Bphone 3",
+        price: 850,
+        quantity: 5
+    }
+]
