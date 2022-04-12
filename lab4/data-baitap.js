9b4072c165dd4c145d9d6b718dda7cf91f5510f9
thienth32 <64114903+thienth32@users.noreply.github.com>
2022-04-12 10:05:33 +0700

    bai 5

---
+++ lab4/data-baitap.js
@@ -0,0 +1,23 @@
+var khachsan = [
+    {
+        id: 1,
+        name: "Metropole",
+        address: "Hà Nội",
+        price: 10000,
+        room: 10
+    },
+    {
+        id: 2,
+        name: "FPT",
+        address: "Hà Nam",
+        price: 15000,
+        room: 15
+    },
+    {
+        id: 3,
+        name: "Poly",
+        address: "Nam Định",
+        price: 20000,
+        room: 100
+    }
+]
