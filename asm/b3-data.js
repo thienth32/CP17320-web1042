995f8490da82cc1148fa23041aecc8833e5d5c83
thienth32 <64114903+thienth32@users.noreply.github.com>
2022-04-15 17:26:36 +0700

    bài 3 tương tác với mảng dữ liệu sanpham

---
+++ asm/b3-data.js
@@ -0,0 +1,23 @@
+var sanpham = [
+    {
+        ma: 1,
+        ten: "iPhone 9",
+        gia: 700,
+        soluong: 0,
+        trangthai: false
+    },
+    {
+        ma: 2,
+        ten: "Samsung",
+        gia: 400,
+        soluong: 0,
+        trangthai: false
+    },
+    {
+        ma: 3,
+        ten: "Nokia",
+        gia: 100,
+        soluong: 0,
+        trangthai: false
+    }
+];
